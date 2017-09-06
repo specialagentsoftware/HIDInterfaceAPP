0777f9365f15a1318511d1df0d885560a5f2f9f8
Jeremy Bond <jebond@trollandtoad.com>
2017-09-06 12:52:43 -0400

    Checking out new dll

--- HIDInterfaceAPP/Class1.cs
+++ HIDInterfaceAPP/Class1.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using HidSharp;
+using RawInput_dll;
+
 
 namespace HIDInterfaceAPP
 {
@@ -16,13 +18,13 @@ namespace HIDInterfaceAPP
                 try
                 {
                     var loader = new HidDeviceLoader();
-                    var device = loader.GetDevices(0X2188,0X0AE1).First();
+                    var device = loader.GetDevices(0X045E,0X00DB).First();
                     HidStream stream;
                     device.TryOpen(out stream);
-                    //string sendme = "<STX>'W'<ETX>";
-                    //System.Text.Encoding enc = System.Text.Encoding.ASCII;
-                   // byte[] messagebyte = enc.GetBytes(sendme);
-                    //stream.Write(messagebyte);
+                    string sendme = "<STX>'W'<ETX>";
+                    System.Text.Encoding enc = System.Text.Encoding.ASCII;
+                    byte[] messagebyte = enc.GetBytes(sendme);
+                    stream.Write(messagebyte);
                     byte[] fromscale = stream.Read();
                     string returnedmessage = enc.GetString(fromscale);
                     return returnedmessage;
