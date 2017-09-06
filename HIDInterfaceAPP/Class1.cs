f421705a1027974bbaf20f9ceeed852d38a9f62e
Jeremy Bond <jebond@trollandtoad.com>
2017-09-06 10:56:18 -0400

    sadasdf

--- HIDInterfaceAPP/Class1.cs
+++ HIDInterfaceAPP/Class1.cs
@@ -16,16 +16,16 @@ namespace HIDInterfaceAPP
                 try
                 {
                     var loader = new HidDeviceLoader();
-                    var device = loader.GetDevices(0X0EB8,0XF000).First();
+                    var device = loader.GetDevices(0X2188,0X0AE1).First();
                     HidStream stream;
                     device.TryOpen(out stream);
-                string sendme = "<STX>'W'<ETX>";
-                System.Text.Encoding enc = System.Text.Encoding.ASCII;
-                byte[] messagebyte = enc.GetBytes(sendme);
-                stream.Write(messagebyte);
-                byte[] fromscale = stream.Read();
-                string returnedmessage = enc.GetString(fromscale);
-                return returnedmessage;
+                    //string sendme = "<STX>'W'<ETX>";
+                    //System.Text.Encoding enc = System.Text.Encoding.ASCII;
+                   // byte[] messagebyte = enc.GetBytes(sendme);
+                    //stream.Write(messagebyte);
+                    byte[] fromscale = stream.Read();
+                    string returnedmessage = enc.GetString(fromscale);
+                    return returnedmessage;
             }
                 catch (Exception e)
                 {
