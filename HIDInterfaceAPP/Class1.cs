636b18d78933e32cfb01580168ef011f5e046037
Jeremy Bond <Jeremy Bond>
2017-09-06 02:13:13 -0400

    Add project files.

---
+++ HIDInterfaceAPP/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HidSharp;
+
+namespace HIDInterfaceAPP
+{
+    class HID
+    {
+        public string Gethid()
+        {
+
+                try
+                {
+                    var loader = new HidDeviceLoader();
+                    var device = loader.GetDevices(0X0EB8,0XF000).First();
+                    HidStream stream;
+                    device.TryOpen(out stream);
+                string sendme = "2'W'3";
+                System.Text.Encoding enc = System.Text.Encoding.ASCII;
+                byte[] messagebyte = enc.GetBytes(sendme);
+                stream.Write(messagebyte);
+                byte[] fromscale = stream.Read();
+                string returnedmessage = enc.GetString(fromscale);
+                return returnedmessage;
+            }
+                catch (Exception e)
+                {
+                    return "false";
+                }
+        }
+    }
+}
---
+++ HIDInterfaceAPP/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace HIDInterfaceAPP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnconnect = new System.Windows.Forms.Button();
+            this.lblstatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnconnect
+            // 
+            this.btnconnect.Location = new System.Drawing.Point(13, 13);
+            this.btnconnect.Name = "btnconnect";
+            this.btnconnect.Size = new System.Drawing.Size(259, 40);
+            this.btnconnect.TabIndex = 0;
+            this.btnconnect.Text = "Connect to HID";
+            this.btnconnect.UseVisualStyleBackColor = true;
+            this.btnconnect.Click += new System.EventHandler(this.btnconnect_Click);
+            // 
+            // lblstatus
+            // 
+            this.lblstatus.AutoSize = true;
+            this.lblstatus.Location = new System.Drawing.Point(13, 84);
+            this.lblstatus.Name = "lblstatus";
+            this.lblstatus.Size = new System.Drawing.Size(46, 13);
+            this.lblstatus.TabIndex = 1;
+            this.lblstatus.Text = "Status --";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.lblstatus);
+            this.Controls.Add(this.btnconnect);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnconnect;
+        private System.Windows.Forms.Label lblstatus;
+    }
+}
+
